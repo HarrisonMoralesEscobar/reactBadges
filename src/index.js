e78f17f0e6b96378620087cbbe4c0b198a05da9c
HarrisonMoralesEscobar <harrymoralese@gmail.com>
2021-06-22 22:40:06 -0500

    Actualizar repo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// import './global.css';
+// import App from './components/App';
+
+// const container = document.getElementById('app');
+
+// ReactDOM.render(<App />, container);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-
+import "bootstrap/dist/css/bootstrap.css";
 import './global.css';
+
+
 import App from './components/App';
+
+
+//const element = <h1>Hello, Platzi Badges!</h1>
+// const element = React.createElement(
+//   'a',
+//   {href: 'https://platzi.com'},
+//   'Ingresa a Platzi..'
+//   );
+
+// const name = 'Nicole';
+// const suma = () => 3+3;
+// const element = React.createElement(
+//   'h1',
+//   {},
+//   `Hola soy ${name}`
+// );
+
+// const jsx = (
+//   <div>
+//     <h1>Hola, soy Harrison</h1>
+//     <p>Soy ingenierdo de software.</p>
+//   </div>
+// );
+
+// const element = React.createElement(
+//   'div',
+//    {},
+//    React.createElement('h1',{},'Hola soy Harrison Morales Escobar'),
+//    React.createElement('p',{},'Soy Ing de Sotware')
+// )
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+// firstName="Harrison"
+// lastName="Morales"
+// jobTitle="Frontend Engineer"
+// avatarUrl="https://s.gravatar.com/avatar/1de368983b54fd73f2fac63d55ad2118?s=80"
+// twitter="harrymoralese"
+
+//ReactDOM.render(__que__ , __donde__);
+ReactDOM.render(
+<App/>,container);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,26 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom'
+import stars from '../images/stars.svg';
+import astro from '../images/astronauts.svg';
 
-import './styles/Home.css';
-import platziconfLogoImage from '../images/platziconf-logo.svg';
-import astronautsImage from '../images/astronauts.svg';
+import './styles/Home.css'
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <div className="container">
-          <div className="row">
-            <div className="Home__col col-12 col-md-4">
-              <img
-                src={platziconfLogoImage}
-                alt="Platzi Conf Logo"
-                className="img-fluid mb-2"
-              />
+export default function Home(){
+  return(
+    
+    <div style={{ backgroundImage: `url(${stars})` }} className="conten">
 
-              <h1>Badge Management System</h1>
-              <Link className="btn btn-primary" to="/badges">
-                Start
-              </Link>
-            </div>
-
-            <div className="Home__col d-none d-md-block col-md-8">
-              <img
-                src={astronautsImage}
-                alt="Astronauts"
-                className="img-fluid p-4"
-              />
-            </div>
-          </div>
-        </div>
+      <div className="content__text">
+        <p className="parrafo">Bienvenidos</p>
+        <Link to="/badges" className="button btn btn-primary">
+          New Start
+        </Link>   
       </div>
-    );
-  }
+
+      <div className="conten__start">
+        <img className="Image_astro" src={astro} alt="logo" />
+      </div>
+
+    </div>
+  );
 }
